3e86412046dfb27afd6f05e8e7a16d1d89c57271
Isaac Fonseca <isaacf8822@gmail.com>
2022-03-24 12:39:13 -0500

    commit tree fef68c84eb061e8d8f12591e0c556c33e790d3d5

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat () {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat () {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift("Milo");
+}
+
+const newcats = ["Broom"]
+function appendCat () {
+    
+    let newcats = [...cats, "Broom"];
+    return newcats;
+}
+
+const newcats2 = ["Arnold"]
+function prependCat () {
+
+    let newcats2 = ["Arnold",...cats];
+    return newcats2;
+}
+
+const newcats3 = ["Milo", "Otis", "Garfield"];
+function removeLastCat () {
+
+    newcats3.splice(2);
+    return newcats3;
+}
+
+const newcats4 = ["Milo", "Otis", "Garfield"];
+function removeFirstCat () {
+
+    newcats4.splice(0,1);
+    return newcats4;
+}
